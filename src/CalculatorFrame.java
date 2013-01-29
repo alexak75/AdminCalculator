be421d8cec739459864a75fb7983e9f038758704
alexak75 <alexak75@gmail.com>
2013-01-29 09:52:19 -0500

    Added a line to calculateFinalPercent()
    The main text field is now cleared when a form is submitted.

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -263,6 +263,8 @@ public class CalculatorFrame extends JFrame {
 		} //end score if statement
 		} //end radiobutton condition if statement
 		} //end of empty score if statement
+		
+		textField_2.setText("");
 	}
 	
 	public void calculateFinalPercent()
